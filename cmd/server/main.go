6e03d2a922bf2bcee5e212406acd490178f228d7
Praneetha Babburi <praneethababburi@Praneethas-MacBook-Air.local>
2026-01-29 10:27:33 -0700

    added handler functions for status update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,8 +14,14 @@ func main() {
 	repo := repository.NewClaimRespository()
 	claimService := service.NewClaimService(repo)
 	claimHandler := handler.NewClaimHandler(claimService)
+
 	r.POST("/claims", claimHandler.CreateClaim)
 	r.GET("/claims/:id", claimHandler.GetClaimByID)
 	r.GET("/claims", claimHandler.GetAllClaims)
+
+	r.PUT("/claims/:id/review", claimHandler.MoveToReview)
+	r.PUT("/claims/:id/approve", claimHandler.ApproveClaim)
+	r.PUT("/claims/:id/deny", claimHandler.DenyClaim)
+
 	r.Run(":8080")
 }
